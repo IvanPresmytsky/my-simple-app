3da937cd9f21b0a3fdd24920ce0613fc4942bd3e
Ivan <ivan.presmytsky@gmail.com>
2026-04-03 15:02:13 +0300

    Add minimal hello world HTTP server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello from my-simple-app!');
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+	res.end('hello World');
+});
+
+server.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
